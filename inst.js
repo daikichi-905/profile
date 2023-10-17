e5fe4cb2a5b54fc8656c8d2ead37af907b3083d6
y.daikichi <yamadai.cell@gmail.com>
2023-10-17 21:43:46 +0900

    10/17変更を追加しました。

--- inst.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const thumbs = document.querySelectorAll('.thumb');
-thumbs.forEach(function(item, index) {
-  item.onclick = function() {
-    document.getElementById('bigimg').src = this.dataset.image;
-  }
-});
--- profile.js
+++ profile.js
@@ -4,3 +4,10 @@ const answer = window.prompt('ホームページを見ますか？');
 if(answer === '見ます') {
   window.alert('ホームページを表示します');
 }
+
+const thumbs = document.querySelectorAll('.thumb');
+thumbs.forEach(function(item, index) {
+  item.onclick = function() {
+    document.getElementById('bigimg').src = this.dataset.image;
+  }
+});
